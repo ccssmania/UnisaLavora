ace0a577bc6401267e8b06be200ca9d84c570c13
ccssmania <ccss@utp.edu.co>
2018-08-09 11:35:48 +0200

    fix register

--- vendor/laravel/framework/src/Illuminate/Foundation/Auth/RegistersUsers.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Auth/RegistersUsers.php
@@ -32,7 +32,7 @@ trait RegistersUsers
 
         event(new Registered($user = $this->create($request->all())));
 
-        $this->guard()->login($user);
+        $this->guard();
 
         return $this->registered($request, $user)
                         ?: redirect($this->redirectPath());
